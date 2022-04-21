518b252288f33b9ba7d80e865bc98bd5ffeac104
Nima <nimarahmati8par@gmail.com>
2022-04-21 12:31:52 +0430

    Small Changes

--- src/components/hero.js
+++ src/components/hero.js
@@ -5,9 +5,9 @@ const Hero = () => {
     return (
         <div className='hero'>
             <h1 data-aos="fade-down" className='hero-text'>
-                Hey! I’m <b><span className="myname">Nima</span></b>,<br />
+                Hey! I’m<b><span className="myname">Nima</span></b>,<br />
                 A <span className='hoze'>Front-end</span> And <br/><span className='hoze'>Back-end </span> 
-                 Developer!
+                Developer!;
             </h1>
             <img data-aos="fade-up-left" src={process.env.PUBLIC_URL + '/meminified.png'} alt="Nima" />
         </div>
